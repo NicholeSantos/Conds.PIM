f3eda341006d565b45ca4559d70aea61db19f0e2
Winicius <winicius.mafra@gmail.com>
2017-10-13 22:38:08 -0300

    Criado o projeto

---
+++ CondS.PIM.Modelo/usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondS.PIM.Modelo
+{
+    public class usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ CondS.PIM.UI.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CondS.PIM.UI.WEB.MvcApplication" Language="C#" %>
